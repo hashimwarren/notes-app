83998cf9e1b95d47b0a0e81085e687735c4cd5b6
Hashim Warren <hashimwarren@gmail.com>
2023-04-19 14:06:18 -0400

    random stuffs

--- notes.js
+++ notes.js
@@ -9,6 +9,8 @@ const addNote = (title, body) => {
     const notes = loadNotes()
     const duplicateNote = notes.find((note) => note.tittle === title)
 
+
+
     if (!duplicateNote) {
         notes.push({
             title: title,
